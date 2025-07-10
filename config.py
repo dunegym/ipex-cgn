ac9c848acff5e897aaadcc8d0b8d31f0d49d2298
dunegym <18162519579@189.cn>
2025-07-11 00:55:05 +0800

    取消nf4量化

--- config.py
+++ config.py
@@ -1,8 +1,13 @@
 import logging
 
-MODEL_LIST = ['DeepSeek-R1-1.5B']
-QUANTIZATION_LIST = ['int8', 'nf4', 'int4']
+LLM_MODEL_LIST = ['DeepSeek-R1-1.5B']
+LLM_DOWNLOAD_LIST = ['Qwen2.5-3B', 
+                     'Qwen2.5-Coder-3B', 
+                     'DeepSeek-R1-7B']
+QUANTIZATION_LIST = ['int8', 
+                     'int4']
 DEVICE_LIST = ['CPU', 'GPU']
+
 
 logging.basicConfig(
     level=logging.INFO,
